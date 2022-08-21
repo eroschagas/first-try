48ba1889d9cbc97b630efe52440773e3f727255a
Eros Chagas <eroschagas@hotmail.com>
2022-08-21 12:30:13 -0300

    testando

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,7 +4,7 @@
             <a
                 href="#"
                 class="poke-card"
-                v-for="(item, index) in items"
+                v-for="(item, index) in itemsSlice"
                 v-bind:key="index"
             >
                 <h1 class="poke-name">{{ item.id }} - {{ item.name }}</h1>
@@ -29,15 +29,32 @@ export default {
         return {
             allPoke: [],
             items: [],
-            n: 0,
+            page: 1,
             maxPoke: false,
+            pokeshow: 21,
         };
+    },
+    computed: {
+        pokemin() {
+            return this.page * this.pokeshow - this.pokeshow;
+        },
+        pokemax() {
+            return this.page * 5 * this.pokeshow + 1;
+        },
+        itemsSlice() {
+            return this.items.slice(this.pokemin, this.pokemin + this.pokeshow);
+        },
+    },
+    watch: {
+        items(newvalue) {
+            this.items = newvalue;
+        },
     },
     methods: {
         fetchData: async function () {
             // await new Promise((r) => setTimeout(r, 100));
             const pokeFetch = [];
-            for (let index = this.n + 1; index < this.n + 22; index++) {
+            for (let index = 1; index < this.pokemax; index++) {
                 try {
                     const response = await fetch(
                         `https://pokeapi.co/api/v2/pokemon/${index}`
@@ -59,26 +76,20 @@ export default {
             this.items = pokeFetch;
         },
         handleNext: function () {
-            let that = this;
             if (this.maxPoke >= true) {
                 return;
             }
-            this.n = this.n + 21;
-
-            setTimeout(function () {
-                that.fetchData();
-            }, 100);
+            this.page++;
+            this.fetchData();
+            console.log("file: Pokemon.vue ~ line 85 ~ this.items", this.items);
         },
         handlePrev: function () {
-            let that = this;
-            if (this.n == 0) {
+            if (this.page == 1) {
                 return;
             }
-            this.n = this.n - 21;
+            this.page--;
             this.maxPoke = false;
-            setTimeout(function () {
-                that.fetchData();
-            }, 500);
+            this.fetchData();
         },
     },
     mounted() {
